750ad75f40b73e66772e2bc4af48ec6029ff5f81
NagiYoshitsugu <nagi.yoshitsugu.0o0@gmail.com>
2021-01-22 13:00:05 +0900

    [Renew] Add compare_extremal

--- frictionData.py
+++ frictionData.py
@@ -4,6 +4,9 @@ import pandas as pd
 import numpy as np
 import requests
 import statsmodels.api as sm
+from sklearn.linear_model import LinearRegression
+from scipy import signal
+from scipy import special
 
 #### Visualize related libraries
 from matplotlib import pylab as plt
@@ -31,7 +34,7 @@ class FrictionData:
     # skipFooter = int(self.time_stop/self.samplingRate)
     
     if self.type == 'sin':
-        self.data = pd.read_csv(self.path, header=None, skiprows=skipHeader, names=['time', 'friction', 'displacement', 'pre1'])
+        self.data = pd.read_csv(self.path, header=None, skiprows=skipHeader, names=['time', 'friction', 'pos', 'pre1'])
     elif self.type == 'square':
         self.data = pd.read_csv(self.path, header=None, skiprows=skipHeader, names=['time', 'friction', 'pre1', 'pre2'])
     else:
@@ -54,13 +57,13 @@ class FrictionData:
     ax1.set_xlabel("time[point]", size = 18, weight = "light")
     ax1.set_ylabel("fce", size = 18, weight = "light")
     ax1.set_ylim(-1*range, range)
-    ax1.plot(self.data['fce'], label="fce", color="#81cac4")
+    ax1.plot(self.data['fce'], label="fce")
     
     if self.type == 'sin':
         ax2 = ax1.twinx()
-        ax2.set_ylabel("displacement", size = 18, weight = "light")
+        ax2.set_ylabel("pos", size = 18, weight = "light")
         ax2.set_ylim(-1*self.length/4, self.length/4)
-        ax2.plot(self.data['displacement'], label="displacement", color="#dd0077")
+        ax2.plot(self.data['pos'], label="pos", color='orange')
     
     plt.tight_layout()
     plt.show()
@@ -72,13 +75,13 @@ class FrictionData:
     ax1.set_xlabel("time[point]", size = 18, weight = "light")
     ax1.set_ylabel("abs_fce", size = 18, weight = "light")
     ax1.set_ylim(0, range)
-    ax1.plot(self.data['abs_fce'], label="abs_fce", color="#81cac4")
+    ax1.plot(self.data['abs_fce'], label="abs_fce")
     
     if self.type == 'sin':
         ax2 = ax1.twinx()
-        ax2.set_ylabel('displacement', size = 18, weight = "light")
+        ax2.set_ylabel('pos', size = 18, weight = "light")
         ax2.set_ylim(-11, 11)
-        ax2.plot(self.data['displacement'], label="displacement", color="#dd0077")
+        ax2.plot(self.data['pos'], label="pos", color='orange')
     
     plt.tight_layout()
     plt.show()
@@ -129,11 +132,21 @@ class FrictionData:
     plt.show()
     
   def calc_v_vs_fce(self):
+    # calc velocity
     if self.type == 'sin':
-        self.data['v'] = self.data['displacement'].diff()/self.samplingRate
+        self.data['v'] = self.data['pos'].diff()/self.samplingRate
+    
+    # make linear regression model
+    v_lr = LinearRegression()
+    v_lr.fit(self.data.dropna()[['v']].values, self.data.dropna()[['fce']].values)
+    
     plt.title(str(self.material)+'-'+str(self.type)+'-'+str(self.speed)+'-'+str(self.length))
     plt.grid(True)
+    plt.xlabel("v[mm/sec]", size = 18, weight = "light")
+    plt.ylabel("fce", size = 18, weight = "light")
     plt.scatter(self.data['v'], self.data['fce'])
+    plt.plot(self.data.dropna()[['v']].values, v_lr.predict(self.data.dropna()[['v']].values), color='orange')
+    plt.tight_layout()
     plt.show()
 
   def adf(self):
@@ -152,5 +165,32 @@ class FrictionData:
     print("---------------------------------------------------------------------------------------------------------------")
     print("nc:")
     print(nc)
+    
+  def compare_extremal(self):
+    index_max_fce = signal.argrelmax(self.data['fce'].values, order = int(self.period/3))
+    index_min_fce = signal.argrelmin(self.data['fce'].values, order = int(self.period/3))
+    
+    if self.type == 'sin':
+        index_max_pos = signal.argrelmax(self.data['pos'].values, order = int(self.period/3))
+        index_min_pos = signal.argrelmin(self.data['pos'].values, order = int(self.period/3))
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+    plt.title(str(self.material)+'-'+str(self.type)+'-'+str(self.speed)+'-'+str(self.length))
+    fig.text(0.2, 0.5, 'mean_maximal : ' + str(self.data['fce'].values[index_max_fce].mean()) + '\n mean_minimal : ' + str(self.data['fce'].values[index_min_fce].mean()), fontsize=16, backgroundcolor='white')
+    ax1.set_xlabel("time[point]", size = 18, weight = "light")
+    ax1.set_ylabel("fce", size = 18, weight = "light")
+    ax1.plot(self.data['time'].values, self.data['fce'].values)
+    ax1.plot(self.data['time'].values[index_max_fce], self.data['fce'].values[index_max_fce], 'o')
+    ax1.plot(self.data['time'].values[index_min_fce], self.data['fce'].values[index_min_fce], 'o')
+    
+    if self.type == 'sin':
+        ax2 = ax1.twinx()
+        ax2.set_ylabel("pos", size = 18, weight = "light")
+        ax2.plot(self.data['time'].values, self.data['pos'].values, color='orange')
+        ax2.plot(self.data['time'].values[index_max_pos], self.data['pos'].values[index_max_pos], 'o', color='red')
+        ax2.plot(self.data['time'].values[index_min_pos], self.data['pos'].values[index_min_pos], 'o', color='blue')
+    
+    plt.show
 
   pass
